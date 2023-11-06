130838369edb6f5f444238ca122ad9d64a37a836
David Štafa <david.stafa@gmail.com>
2023-11-06 14:59:13 +0100

    react setup, html code, styling

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"> 
+    <title>Laravel + React</title>
+    <style>
+          @import url("https://fonts.googleapis.com/css2?family=Cutive+Mono&family=Noto+Serif+Display:wght@200;400;700&display=swap");
+    </style>
+    @vite('resources/css/homepage.scss')
+</head>
+    <body>
+    
+    <div id="people-of-interest-app"></div>
+    
+    @vite('resources/js/people_of_interest.jsx')
+
+
+
+    </body>
+</html>
