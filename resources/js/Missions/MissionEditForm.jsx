835381e72816e44b1ea730c7d68c164c50917f6d
Jakub Kozler <kozlerjakub@gmail.com>
2023-11-08 09:28:54 +0100

    last

--- resources/js/Missions/MissionEditForm.jsx
+++ resources/js/Missions/MissionEditForm.jsx
@@ -2,8 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function MissionEditForm({ missionId, setMissionId }) {
-    
-    const [missionDetails, setMissionDetails] = useState("");
+    const [missionDetails, setMissionDetails] = useState({});
 
     const loadMission = async () => {
         try {
@@ -20,7 +19,7 @@ export default function MissionEditForm({ missionId, setMissionId }) {
         loadMission();
     }, [missionId]);
 
-// *** Handling the form *** 
+    // *** Handling the form ***
 
     // const [values, setValues] = useState({
     //     name: '',
@@ -29,41 +28,61 @@ export default function MissionEditForm({ missionId, setMissionId }) {
     // });
 
     const handleChange = (event) => {
-    setMissionDetails(previous_values => {
-        return ({...previous_values, 
-            [event.target.name]: event.target.value
+        setMissionDetails((previous_values) => {
+            return {
+                ...previous_values,
+                [event.target.name]: event.target.value,
+            };
         });
-    });
-}
+    };
 
-// *** END of Handling the form *** 
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+    };
+
+    // *** END of Handling the form ***
 
     return (
         <>
             <h1>Edit mission{missionDetails.id}</h1>
-    {           
-        missionDetails &&
-            <div className="mission_form">
-                <form action='' onSubmit={handleChange}>
+            {missionDetails && (
+                <div className="mission_form">
+                    <form action="" onSubmit={handleSubmit}>
+                        <label htmlFor="name">Name:</label>
+                        <input
+                            type="text"
+                            name="name"
+                            id="name"
+                            value={missionDetails.name}
+                            onChange={handleChange}
+                        />
 
-                    <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" id="name" value={missionDetails.name} onChange={handleChange}  />
+                        <label htmlFor="year">Year:</label>
+                        <input
+                            type="text"
+                            name="year"
+                            id="year"
+                            value={missionDetails.year}
+                            onChange={handleChange}
+                        />
 
-                    <label htmlFor="year">Year:</label>
-                    <input type="text" name="year" id="year" value={missionDetails.year} onChange={handleChange} />
+                        <label htmlFor="outcome">Outcome:</label>
+                        <select
+                            name="outcome"
+                            id="outcome"
+                            value={missionDetails.outcome}
+                            onChange={handleChange}
+                        >
+                            <option value={true}>Succesfull</option>
+                            <option value={false}>Failed</option>
+                            <option value={null}>Ongoing</option>
+                        </select>
 
-                    <label htmlFor="outcome">Outcome:</label>
-                    <select name="outcome" id="outcome" value={missionDetails.outcome} onChange={handleChange}  >
-                        <option value={true} >Succesfull</option>
-                        <option value={false} >Failed</option>
-                        <option value={null} >Ongoing</option>
-                    </select>
-
-                    <button type="submit">Update</button>
-                </form>
-            </div>
-    }
-        <button onClick={ () => setMissionId(null) }>&times;</button>
+                        <button type="submit">Update</button>
+                    </form>
+                </div>
+            )}
+            <button onClick={() => setMissionId(null)}>&times;</button>
         </>
     );
 }
